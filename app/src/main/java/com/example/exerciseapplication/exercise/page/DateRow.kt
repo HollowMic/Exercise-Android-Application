99beb578d7a258da7b9934153df3354b8731eaaa
Jamie Thomas <jth141@uclive.ac.nz>
2024-06-29 15:35:13 +1200

    Added date picker (to be implemented)

--- app/src/main/java/com/example/exerciseapplication/exercise/page/DateRow.kt
+++ app/src/main/java/com/example/exerciseapplication/exercise/page/DateRow.kt
@@ -1,49 +1,89 @@
 package com.example.exerciseapplication.exercise.page
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.DateRange
+import androidx.compose.material3.Card
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.time.Instant
 import java.util.Date
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DateRow(modifier: Modifier = Modifier) {
     Box {
+        var expanded by remember { mutableStateOf(false) }
+        fun toggleExpanded() {
+            expanded = !expanded
+        }
         Surface(
-            modifier = Modifier.fillMaxWidth().height(60.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(if (expanded) 570.dp else 60.dp)
+                .animateContentSize(),
             color = MaterialTheme.colorScheme.primaryContainer,
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth().height(20.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
+            Column(
+                verticalArrangement = Arrangement.Top
             ) {
-                val date = Date.from(Instant.now()).toString().subSequence(0, 10).toString()
-                Text(text = date, modifier = Modifier.padding(20.dp, 0.dp))
-                IconButton(
-                    modifier = Modifier.padding(20.dp, 0.dp),
-                    onClick = { /*TODO*/ }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(60.dp),
+//                        .padding(0.dp, 10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    Icon(imageVector = Icons.Rounded.DateRange, "")
+                    val date = Date.from(Instant.now()).toString().subSequence(0, 10).toString()
+                    Text(text = date, modifier = Modifier.padding(20.dp, 0.dp))
+                    IconButton(
+                        modifier = Modifier.padding(20.dp, 0.dp),
+                        onClick = { toggleExpanded() }
+                    ) {
+                        Icon(imageVector = Icons.Rounded.DateRange, "")
+                    }
+                }
+                val dateState = rememberDatePickerState()
+                if (expanded) {
+                    Card(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.secondaryContainer)
+                            .padding(10.dp)
+                    ) {
+                        DatePicker(state = dateState)
+                    }
                 }
             }
+
         }
+
     }
 }
 
